f1369a1f31d987e1bfe0004e5afdf2cc81ff00dd
Abdur Rahman <rubel100@gmail.com>
2015-04-30 21:15:44 -0400

    edit category

--- public/scripts/controller/category.js
+++ public/scripts/controller/category.js
@@ -12,10 +12,33 @@ routerApp.controller('CategoryCtrl', function($scope, $http) {
 
 		$http.get('http://localhost:8888/resful/public/v1/categories/'+ $stateParams.id).
 		  	success(function(data, status) {
-		  		console.log(data);
-		  		$scope.formData = data;
+ 		  		$scope.formData = data;
 		  	}).
 		  	error(function(data, status) {
 		  		Logger.debug(data);
 		  	});
+
+		  	$scope.submitData = function() 
+			{
+				$scope.showSubmit = false;
+				
+				if ($scope.formData)
+				{
+					var resourceURL = 'http://localhost:8888/resful/public/v1/categories/'+ $stateParams.id;
+
+ 					var formData = $scope.formData;
+ 
+					$http.put(resourceURL, formData).
+					  	success(function(data, status) {
+					  		$scope.errors = null;
+							$scope.success = 'Your data has been saved';
+							$scope.showSubmit = true;
+							$state.go('category');
+					  	}).
+					  	error(function(data, status) {
+					  		$scope.errors = data.error.message;
+							$scope.showSubmit = true;
+					  	});
+				}
+			}
 	}]);
